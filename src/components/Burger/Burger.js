c35d6aa7f0af60de3ed1aa6f4477fd4406e00e72
DijkstraEdsger <humbertomartinez900519@gmail.com>
2021-02-04 11:26:40 -0500

    output ingredients in burger that comes from wrapper component

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React from "react";
+import PropTypes from "prop-types";
 
-import classes from './Burger.module.css';
-import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+import classes from "./Burger.module.css";
+import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const burger = (props) => {
-    return (
-        <div className={classes.Burger}>
-            <BurgerIngredient type="bread-top"/>
-            <BurgerIngredient type="cheese"/>
-            <BurgerIngredient type="meat"/>
-            <BurgerIngredient type="bread-bottom"/>
-        </div>
-    );
-}
+  const transformedIngredients = Object.keys(props.ingredients).map((igKey) => {
+    return [...Array(props.ingredients[igKey])].map((_, i) => {
+      return <BurgerIngredient key={igKey + i} type={igKey} />;
+    });
+  });
+  return (
+    <div className={classes.Burger}>
+      <BurgerIngredient type="bread-top" />
+      {transformedIngredients}
+      <BurgerIngredient type="bread-bottom" />
+    </div>
+  );
+};
+
+burger.propTypes = {
+  ingredients: PropTypes.object,
+};
 
 export default burger;
