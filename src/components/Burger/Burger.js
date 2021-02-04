639048fffdc5ec6bf8c0fa4aa9d79f01030439f3
DijkstraEdsger <humbertomartinez900519@gmail.com>
2021-02-04 13:10:20 -0500

    working in build burger controls

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,13 +3,16 @@ import PropTypes from "prop-types";
 
 import classes from "./Burger.module.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
+import BuildControls from "./BuildControls/BuildControls";
 
 const burger = (props) => {
-  let transformedIngredients = Object.keys(props.ingredients).map((igKey) => {
-    return [...Array(props.ingredients[igKey])].map((_, i) => {
-      return <BurgerIngredient key={igKey + i} type={igKey} />;
-    });
-  }).reduce((arr, c) => arr.concat(c), []);
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map((igKey) => {
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        return <BurgerIngredient key={igKey + i} type={igKey} />;
+      });
+    })
+    .reduce((arr, c) => arr.concat(c), []);
 
   if (transformedIngredients.length === 0) {
     transformedIngredients = <p>Please start adding ingredients!</p>;
@@ -20,6 +23,7 @@ const burger = (props) => {
       <BurgerIngredient type="bread-top" />
       {transformedIngredients}
       <BurgerIngredient type="bread-bottom" />
+      <BuildControls />
     </div>
   );
 };
