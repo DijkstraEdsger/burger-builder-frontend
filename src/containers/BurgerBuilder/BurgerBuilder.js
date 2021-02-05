03994a356f9a600b0e4fac145c7cb9f25ae9a6ae
DijkstraEdsger <humbertomartinez900519@gmail.com>
2021-02-05 16:09:12 -0500

    order post request

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import Burger from "../../components/Burger/Burger";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
+import axios from "../../axios-orders";
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -35,7 +36,27 @@ class BurgerBuilder extends Component {
   };
 
   purchaseContinueHandler = () => {
-    alert("You continue!");
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice,
+      customer: {
+        name: "Humberto",
+        address: {
+          street: "any",
+          zipCode: 80100,
+          country: "Somewhere",
+        },
+        email: "email@gmail.com",
+      },
+      deliveryMethod: "kjl",
+    };
+
+    axios
+      .post("/orders.json", order)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => console.log(error));
   };
 
   updatePurchaseState(ingredients) {
